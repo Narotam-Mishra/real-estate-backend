db39406dbf2dff28457f87e1c0164de8d1c04980
Narotam-Mishra <narotam21jan@gmail.com>
2024-07-22 10:57:42 +0530

    real-estate-backend_1.16_fetch_profile_posts

--- controllers/userController.js
+++ controllers/userController.js
@@ -113,3 +113,29 @@ export const savePost = async (req, res) => {
         res.status(500).json({ message: "Failed to delete user!!" })
     }
 }
+
+export const profilePosts = async (req, res) => {
+    const tokenUserId = req.params.userId
+
+    try {
+        const userPosts = await prisma.post.findMany({
+            where: {
+                userId: tokenUserId
+            }
+        })
+
+        const saved = await prisma.savedPost.findMany({
+            where: {
+                userId: tokenUserId
+            },
+            include: {
+                post: true,
+            }
+        })
+        const savedPosts = saved.map((item) => item.post)
+        res.status(200).json({ userPosts, savePost });
+    } catch (error) {
+        console.log("Error while getting user:", error)
+        res.status(500).json({ message: "Failed to get profile posts!" })
+    }
+}
